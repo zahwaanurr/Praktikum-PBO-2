cf645d636140cde55419c0dd4174397213af588c
zahwaanurr <145866169+zahwaanurr@users.noreply.github.com>
2024-03-15 13:55:07 +0700

    Delete prak2e2.py

--- prak2e2.py
+++
@@ -1,8 +0,0 @@
-nim = "38"  
-two_last_digits = int(nim[-2:])
-
-print("Deret angka dari 1 sampai 50 tanpa dua digit terakhir NIM:")
-for i in range(1, 51):
-    if i % 100 != two_last_digits:
-        print(i, end=" ")
-print()
